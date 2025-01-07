522269e279762943e1b65b24e4614bc627d36650
Ayu5h5hakya <qwerty.ayush5@gmail.com>
2025-01-07 11:56:08 +0545

    language selection page refactor

--- lib/features/language/presentation/screen/language_selection_screen.dart
+++ lib/features/language/presentation/screen/language_selection_screen.dart
@@ -1,10 +1,8 @@
-import 'package:aid_ready/core/domain/entity/locale_option.dart';
-import 'package:aid_ready/core/theme/color.dart';
 import 'package:aid_ready/core/theme/styles.dart';
-import 'package:aid_ready/core/utils/extensions/type.dart';
+import 'package:aid_ready/core/utils/extensions/context.dart';
 import 'package:aid_ready/core/utils/extensions/ui.dart';
 import 'package:aid_ready/core/widgets/action_button.dart';
-import 'package:aid_ready/features/language/presentation/widgets/language_item_row.dart';
+import 'package:aid_ready/features/language/presentation/widgets/language_list.dart';
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 
@@ -17,11 +15,12 @@ class LanguageSelectionScreen extends StatelessWidget {
     return Scaffold(
       body: Stack(
         children: [
-          LanguageList(),
+          const LanguageList(),
           Container(
             margin: const EdgeInsets.only(bottom: 64.0),
             alignment: Alignment.bottomCenter,
             child: ActionButton.primary(
+              height: context.h(48.0),
               child: Center(
                 child: Text(
                   'Next',
@@ -32,51 +31,6 @@ class LanguageSelectionScreen extends StatelessWidget {
           )
         ],
       ).px(16.0),
-    );
-  }
-}
-
-class LanguageList extends StatefulWidget {
-  const LanguageList({
-    super.key,
-  });
-
-  @override
-  State<LanguageList> createState() => _LanguageListState();
-}
-
-class _LanguageListState extends State<LanguageList> {
-  int _selected = 0;
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        Text(
-          'Select Language',
-          style: regular.copyWith(fontSize: 24.0, color: primaryDark950),
-        ),
-        24.verticalSpace,
-        LanguageItemRow(
-          locale: LocaleOpts.english,
-          selected: _selected == 0,
-          onSelected: () {
-            setState(() {
-              _selected = 0;
-            });
-          },
-        ),
-        16.verticalSpace,
-        LanguageItemRow(
-          locale: LocaleOpts.nepali,
-          selected: _selected == 1,
-          onSelected: () {
-            setState(() {
-              _selected = 1;
-            });
-          },
-        ),
-      ],
     );
   }
 }
---
+++ lib/features/language/presentation/widgets/language_list.dart
@@ -0,0 +1,51 @@
+import 'package:aid_ready/core/domain/entity/locale_option.dart';
+import 'package:aid_ready/core/theme/color.dart';
+import 'package:aid_ready/core/theme/styles.dart';
+import 'package:aid_ready/core/utils/extensions/type.dart';
+import 'package:aid_ready/features/language/presentation/widgets/language_item_row.dart';
+import 'package:flutter/material.dart';
+
+class LanguageList extends StatefulWidget {
+  const LanguageList({
+    super.key,
+  });
+
+  @override
+  State<LanguageList> createState() => _LanguageListState();
+}
+
+class _LanguageListState extends State<LanguageList> {
+  int _selected = 0;
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Text(
+          'Select Language',
+          style: regular.copyWith(fontSize: 24.0, color: primaryDark950),
+        ),
+        24.verticalSpace,
+        LanguageItemRow(
+          locale: LocaleOpts.english,
+          selected: _selected == 0,
+          onSelected: () {
+            setState(() {
+              _selected = 0;
+            });
+          },
+        ),
+        16.verticalSpace,
+        LanguageItemRow(
+          locale: LocaleOpts.nepali,
+          selected: _selected == 1,
+          onSelected: () {
+            setState(() {
+              _selected = 1;
+            });
+          },
+        ),
+      ],
+    );
+  }
+}

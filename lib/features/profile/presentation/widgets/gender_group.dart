a255932d49003bbdb504ecf2f8cb79628ae168b9
Ayu5h5hakya <qwerty.ayush5@gmail.com>
2025-01-10 15:24:01 +0545

    make gender group interactive

--- lib/features/profile/presentation/widgets/gender_group.dart
+++ lib/features/profile/presentation/widgets/gender_group.dart
@@ -18,70 +18,96 @@ class _GenderGroupState extends State<GenderGroup> {
     return Row(
       mainAxisAlignment: MainAxisAlignment.start,
       children: [
-        Container(
-          decoration: BoxDecoration(borderRadius: BorderRadius.circular(8.0)),
-          child: Row(
-            children: [
-              Radio(
-                value: _selected == 0,
-                onChanged: (value) {
-                  if (value ?? false) {
+        GestureDetector(
+          onTap: () {
+            setState(() {
+              _selected = 0;
+            });
+          },
+          child: Container(
+            decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(8.0),
+                border: Border.all(width: 1.0, color: primaryDark50)),
+            margin: const EdgeInsets.only(right: 10.0),
+            child: Row(
+              children: [
+                Radio(
+                  activeColor: primary500,
+                  value: _selected == 0,
+                  onChanged: (value) {
                     setState(() {
                       _selected = 0;
                     });
-                  }
-                },
-                groupValue: true,
-              ),
-              Text(
-                context.l10n.male,
-                style: regular.copyWith(color: primaryDark950),
-              )
-            ],
+                  },
+                  groupValue: true,
+                ),
+                Text(
+                  context.l10n.male,
+                  style: regular.copyWith(color: primaryDark950),
+                )
+              ],
+            ),
           ),
         ),
-        Container(
-          decoration: BoxDecoration(borderRadius: BorderRadius.circular(8.0)),
-          child: Row(
-            children: [
-              Radio(
-                value: _selected == 1,
-                onChanged: (value) {
-                  if (value ?? false) {
+        GestureDetector(
+          onTap: () {
+            setState(() {
+              _selected = 1;
+            });
+          },
+          child: Container(
+            decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(8.0),
+                border: Border.all(width: 1.0, color: primaryDark50)),
+            margin: const EdgeInsets.only(right: 10.0),
+            child: Row(
+              children: [
+                Radio(
+                  activeColor: primary500,
+                  value: _selected == 1,
+                  onChanged: (value) {
                     setState(() {
                       _selected = 1;
                     });
-                  }
-                },
-                groupValue: true,
-              ),
-              Text(
-                context.l10n.female,
-                style: regular.copyWith(color: primaryDark950),
-              )
-            ],
+                  },
+                  groupValue: true,
+                ),
+                Text(
+                  context.l10n.female,
+                  style: regular.copyWith(color: primaryDark950),
+                )
+              ],
+            ),
           ),
         ),
-        Container(
-          decoration: BoxDecoration(borderRadius: BorderRadius.circular(8.0)),
-          child: Row(
-            children: [
-              Radio(
-                value: _selected == 2,
-                onChanged: (value) {
-                  if (value ?? false) {
+        GestureDetector(
+          onTap: () {
+            setState(() {
+              _selected = 2;
+            });
+          },
+          child: Container(
+            decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(8.0),
+                border: Border.all(width: 1.0, color: primaryDark50)),
+            child: Row(
+              children: [
+                Radio(
+                  activeColor: primary500,
+                  value: _selected == 2,
+                  onChanged: (value) {
                     setState(() {
                       _selected = 2;
                     });
-                  }
-                },
-                groupValue: true,
-              ),
-              Text(
-                context.l10n.others,
-                style: regular.copyWith(color: primaryDark950),
-              )
-            ],
+                  },
+                  groupValue: true,
+                ),
+                Text(
+                  context.l10n.others,
+                  style: regular.copyWith(color: primaryDark950),
+                )
+              ],
+            ),
           ),
         )
       ],
